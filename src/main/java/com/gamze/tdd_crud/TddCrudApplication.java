aae35ddf0441257758ed06676a796a2f0c96e91f
Gamze Ercan <gamzeipek06@gmail.com>
2025-12-13 19:19:08 +0100

    started to tdd crud application

---
+++ src/main/java/com/gamze/tdd_crud/TddCrudApplication.java
@@ -0,0 +1,13 @@
+package com.gamze.tdd_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamze/tdd_crud/TddCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamze.tdd_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
